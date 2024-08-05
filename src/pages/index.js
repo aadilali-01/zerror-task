0a17445d1b6b7355cd0fdb263b523e7141cea3a8
aadilali-01 <aa2516203@gmail.com>
2024-08-06 01:31:32 +0530

    Completed

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,118 +1,85 @@
+import gsap from "gsap";
+import ScrollTrigger from "gsap/dist/ScrollTrigger";
 import Image from "next/image";
-import { Inter } from "next/font/google";
+import React, { useEffect, useRef, useState } from "react";
 
-const inter = Inter({ subsets: ["latin"] });
+import image1 from "@/assets/images/image1.png"
+import image2 from "@/assets/images/image2.png"
 
-export default function Home() {
+const marqueeData = Array(12).fill(image1);
+const marqueeData2 = Array(12).fill(image2);
+
+const Home = () => {
+  gsap.registerPlugin(ScrollTrigger);
+
+  const slider = useRef(null);
+  const firstSlider = useRef(null);
+  const secondSlider = useRef(null);
+  let xPercentLeft = 0;
+  let xPercentRight = 100;
+  let direction = -1;
+  let speed = 0.1;
+  let animationFrameId;
+
+  const [scroll, setScroll] = useState(0);
+  const timeoutRef = useRef(null);
+
+  const handleScroll = () => {
+    setScroll(window.scrollY);
+    speed = 0.5;
+    clearTimeout(timeoutRef.current);
+    timeoutRef.current = setTimeout(() => {
+      speed = 0.1;
+    }, 100);
+  };
+
+  const animateSliders = () => {
+    xPercentLeft = xPercentLeft <= -100 ? 0 : xPercentLeft + speed * direction;
+    xPercentRight = xPercentRight <= 0 ? 100 : xPercentRight + speed * direction;
+
+    gsap.set(firstSlider.current, { xPercent: xPercentLeft });
+    gsap.set(secondSlider.current, { xPercent: -xPercentRight });
+
+    animationFrameId = requestAnimationFrame(animateSliders);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    animateSliders();
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+      cancelAnimationFrame(animationFrameId);
+    };
+  }, []);
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <div className="h-[50vh]"></div>
+      <div className="w-100 bg-black overflow-hidden relative">
+        <div className="relative w-100" ref={slider}>
+          <div ref={firstSlider} className="flex my-10">
+            {marqueeData.map((src, index) => (
+              <div className="flex items-center gap-3 px-10" key={index}>
+                <div className="relative w-56 h-36">
+                  <Image className="object-contain opacity-50 transition-opacity duration-150 hover:opacity-100 cursor-pointer" src={src} alt="ars technica logo" fill />
+                </div>
+              </div>
+            ))}
+          </div>
+          <div ref={secondSlider} className="flex my-10">
+            {marqueeData2.map((src, index) => (
+              <div className="flex items-center gap-3 px-10" key={index}>
+                <div className="relative w-56 h-36">
+                  <Image className="object-contain opacity-50 transition-opacity duration-150 hover:opacity-100 cursor-pointer" src={src} alt="Glamour logo" fill />
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </>
   );
-}
+};
+
+export default Home;
